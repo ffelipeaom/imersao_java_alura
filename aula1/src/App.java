1c5160340fcc0acf45ff43b0d3f19c00df26941b
Felipe Augusto Oliveira Menezes <95704769+ffelipeaom@users.noreply.github.com>
2022-07-21 15:24:55 -0300

    Add files via upload

---
+++ aula1/src/App.java
@@ -0,0 +1,34 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        //System.out.println("Hello, World!");
+
+        //fazer conexão HTTP e buscar os top 250 filmes
+        String url = "https://alura-imdb-api.herokuapp.com/movies";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
+
+        //parsear: extrair os dados que interessam (título, imagem, nota)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+        //exibit e manipular os dados
+        for (Map<String,String> filme : listaDeFilmes) {
+            System.out.println(filme.get("title"));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
+    }
+}
