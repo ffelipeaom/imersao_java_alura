fcc9c6fd13757084b28dc2e9f72e521fd040fa9f
Felipe Augusto Oliveira Menezes <95704769+ffelipeaom@users.noreply.github.com>
2022-07-25 15:43:50 -0300

    Add files via upload

---
+++ aula4/alura-stickers/src/Conteudo.java
@@ -0,0 +1,19 @@
+public class Conteudo {
+    private final String titulo;
+    private final String urlImagem;
+
+    public Conteudo(String t, String urlImagem) {
+        this.titulo = t;
+        this.urlImagem = urlImagem;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }    
+    public String getUrlImagem() {
+        return urlImagem;
+    }
+
+    
+    
+}
